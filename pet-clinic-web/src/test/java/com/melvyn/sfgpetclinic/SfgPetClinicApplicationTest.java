f78d67587d06fab3355bf9cef31284821299f3ab
papashep <melvyn.shepherd@outlook.com>
2020-08-13 12:42:40 +0100

    Refactoring to MultiModule Maven Project. closes #1
    Signed-off-by: papashep <melvyn.shepherd@outlook.com>

--- pet-clinic-web/src/test/java/com/melvyn/sfgpetclinic/SfgPetClinicApplicationTest.java
+++ pet-clinic-web/src/test/java/com/melvyn/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -3,12 +3,14 @@ package com.melvyn.sfgpetclinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+/**
+ * Created by Melvyn on 13/Aug/2020
+ */
 @SpringBootTest
-class SfgPetClinicApplicationTests {
+class SfgPetClinicApplicationTest {
 
     @Test
+//    void main () {
     void contextLoads () {
     }
-
 }
