efb99659f284ac64bcd47ddf47a1e584f96ff718
papashep <melvyn.shepherd@outlook.com>
2020-08-16 17:33:59 +0100

    Adding vets page and controller. Closes #7
    Signed-off-by: papashep <melvyn.shepherd@outlook.com>

---
+++ pet-clinic-web/src/main/java/com/melvyn/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,18 @@
+package com.melvyn.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Melvyn on 16/Aug/2020
+ */
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+
+        return "vets/index";
+    }
+
+}
