65c8a14fa6dbdba3068938109b6fe2612fba3d33
papashep <melvyn.shepherd@outlook.com>
2020-08-16 17:04:57 +0100

    Adding the index.html page and controller. Closes #8
    Signed-off-by: papashep <melvyn.shepherd@outlook.com>

---
+++ pet-clinic-web/src/main/java/com/melvyn/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.melvyn.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Melvyn on 16/Aug/2020
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+
+        return "index";
+    }
+}
