30583eae6233be40a04e41ccc92f15ad0ef5da9f
papashep <melvyn.shepherd@outlook.com>
2020-08-19 13:02:01 +0100

    Refactored Map Impl to Abstract Id generation. Closes #23
    Signed-off-by: papashep <melvyn.shepherd@outlook.com>

--- pet-clinic-web/src/main/java/com/melvyn/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/melvyn/sfgpetclinic/bootstrap/DataLoader.java
@@ -30,34 +30,42 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run (String... args) throws Exception {
         Owner owner1 = new Owner ();
-        owner1.setId (1L);
         owner1.setFirstName ("Michael");
         owner1.setLastName ("Weston");
 
         ownerService.save (owner1);
 
         Owner owner2 = new Owner ();
-        owner2.setId (2L);
         owner2.setFirstName ("Fiona");
         owner2.setLastName ("Glenanne");
 
         ownerService.save (owner2);
 
+        Owner owner3 = new Owner ();
+        owner3.setFirstName ("Angela");
+        owner3.setLastName ("Shepherd");
+
+        ownerService.save (owner3);
+
         System.out.println ("Loaded Owners....");
 
         Vet vet1 = new Vet ();
-        vet1.setId (1L);
         vet1.setFirstName ("Sam");
         vet1.setLastName ("Axe");
 
         vetService.save (vet1);
 
         Vet vet2 = new Vet ();
-        vet2.setId (2L);
         vet2.setFirstName ("Jessie");
         vet2.setLastName ("Porter");
 
         vetService.save (vet2);
+
+        Vet vet3 = new Vet ();
+        vet3.setFirstName ("Martin");
+        vet3.setLastName ("Shepherd");
+
+        vetService.save (vet3);
 
         System.out.println ("Loaded Vets....");
 
