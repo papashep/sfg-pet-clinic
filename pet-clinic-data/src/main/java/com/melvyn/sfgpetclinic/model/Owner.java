fc1d27f00363b790deee2f5f402286ae1d609971
papashep <melvyn.shepherd@outlook.com>
2020-08-24 13:45:29 +0100

    Added visits, updated Owner with pet relationship. Closes #26
    Signed-off-by: papashep <melvyn.shepherd@outlook.com>

--- pet-clinic-data/src/main/java/com/melvyn/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/melvyn/sfgpetclinic/model/Owner.java
@@ -1,8 +1,19 @@
 package com.melvyn.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Melvyn on 13/Aug/2020
  */
 public class Owner extends Person {
 
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets () {
+        return pets;
+    }
+
+    public void setPets (Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
