819bdb910902691b36b88e0673a19872a8a6e52d
papashep <melvyn.shepherd@outlook.com>
2020-08-29 14:48:20 +0100

    Add Visit entity. Closes #38
    Signed-off-by: papashep <melvyn.shepherd@outlook.com>

--- pet-clinic-data/src/main/java/com/melvyn/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/melvyn/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.melvyn.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Melvyn on 13/Aug/2020
@@ -24,6 +26,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName () {
         return name;
@@ -55,5 +60,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate (LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits () {
+        return visits;
+    }
+
+    public void setVisits (Set<Visit> visits) {
+        this.visits = visits;
     }
 }
--- pet-clinic-data/src/main/java/com/melvyn/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/melvyn/sfgpetclinic/model/Visit.java
@@ -1,14 +1,23 @@
 package com.melvyn.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by Melvyn on 24/Aug/2020
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate () {
