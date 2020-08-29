2a593596911349293bb120af1820ea566f72702d
papashep <melvyn.shepherd@outlook.com>
2020-08-29 14:09:07 +0100

    Updated Vet to use JPA. Closes #37
    Signed-off-by: papashep <melvyn.shepherd@outlook.com>

--- pet-clinic-data/src/main/java/com/melvyn/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/melvyn/sfgpetclinic/model/Speciality.java
@@ -1,10 +1,17 @@
 package com.melvyn.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by Melvyn on 24/Aug/2020
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+   @Column(name = "description")
     private String description;
 
     public String getDescription () {
--- pet-clinic-data/src/main/java/com/melvyn/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/melvyn/sfgpetclinic/model/Vet.java
@@ -1,13 +1,20 @@
 package com.melvyn.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by Melvyn on 13/Aug/2020
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>(); // Initialize this to stop a NullPointerException
 
     public Set<Speciality> getSpecialities () {
