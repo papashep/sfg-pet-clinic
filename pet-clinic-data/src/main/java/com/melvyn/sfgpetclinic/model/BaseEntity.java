f2b8f025dfe430c29d49a57c9648b69293e84bb1
papashep <melvyn.shepherd@outlook.com>
2020-08-29 11:51:47 +0100

    Updated BaseEntity with JPA annotations. Closes #35.
    Signed-off-by: papashep <melvyn.shepherd@outlook.com>

--- pet-clinic-data/src/main/java/com/melvyn/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/melvyn/sfgpetclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package com.melvyn.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by Melvyn on 15/Aug/2020
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;    // Recommendation of hibernate is to use box types and not primitive, box types can be
                         // and primitives can not be.
 
